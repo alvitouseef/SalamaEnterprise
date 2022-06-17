802a1b51b0a1d9a256f1666c979f6d2a0b4aa108
asad <asad@ASAD-PC>
2022-06-17 10:21:03 +0500

    Add project files.

---
+++ SalamaEnterprise/Controllers/FormController.cs
@@ -0,0 +1,47 @@
+﻿using SalamaEnterprise.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SalamaEnterprise.Controllers
+{
+
+    public class FormController : Controller
+    {
+        FormFields formFld = new Models.FormFields();
+        // GET: Form
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult CarDetails()
+        {
+            return View();
+        }
+
+        public ActionResult CarInformation()
+        {
+            return View();
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public JsonResult dropDownFiller(string key, string param)
+        {
+            try
+            {
+                return Json(formFld.dropDownFiller(key, param), JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                if (Request.IsAjaxRequest())
+                {
+                    Response.StatusCode = 500;
+                    return Json("Error in " + ex.Message.ToString(), JsonRequestBehavior.AllowGet);
+                }
+                return Json(ex.Message, JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
---
+++ SalamaEnterprise/Models/FormFields.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace SalamaEnterprise.Models
+{
+    public class FormFields
+    {
+        public Array dropDownFiller(string key, string param)
+        {
+            string query = "";
+            if (key != null || key != "")
+            {
+                query = DBQueries.getDDLQuery(key, param);
+            }
+
+
+            ArrayList fetchInner;
+            ArrayList fetchOuter = new ArrayList();
+            string str_TempValue = null;
+            using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["SalamaDB"].ConnectionString/*DBConnection.connectionString_OLEDB*/))
+            {
+                try
+                {
+                    connection.Open();
+                    SqlCommand com = new SqlCommand(query, connection);
+                    SqlDataReader rdr = com.ExecuteReader();
+                    DataTable schema = rdr.GetSchemaTable();
+                    fetchInner = new ArrayList();
+                    while (rdr.Read())
+                    {
+                        fetchInner = new ArrayList();
+                        for (int i = 0; i < schema.Rows.Count; i++)
+                        {
+                            str_TempValue = rdr.GetValue(i).ToString();
+                            fetchInner.Add(str_TempValue);
+                        }
+                        fetchOuter.Add(fetchInner.ToArray());
+                    }
+                    rdr.Close();
+                }
+                catch (System.Exception ex)
+                {
+                    throw new Exception(ex.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+                return fetchOuter.ToArray();
+            }
+
+        }
+    }
+}
